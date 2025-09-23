60c605ab8bbf6e709a21e250eceabb39490c5932
vucoffee2310 <vucoffee2310@gmail.com>
2025-09-24 00:06:17 +0700

    Delete 1.0.4_0 directory

--- 1.0.4_0/js/background.js
+++
@@ -1,9 +0,0 @@
-chrome.action.onClicked.addListener(tab => {
-    chrome.windows.create({
-        type: "popup",
-        url: `html/popup.html?tabId=${tab.id}`,
-        focused: true,
-        width: 310,
-        height: 320
-    });
-});

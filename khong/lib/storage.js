cc8ad67affe3df36f97c7717d792d9d5e4a46501
vucoffee2310 <vucoffee2310@gmail.com>
2025-09-28 07:53:40 +0700

    Delete khong/lib directory

--- khong/lib/storage.js
+++
@@ -1,9 +0,0 @@
-// lib/storage.js
-export const getStorage = async (key) => {
-  const result = await chrome.storage.local.get(key);
-  return result[key];
-};
-
-export const setStorage = (key, value) => chrome.storage.local.set({ [key]: value });
-
-export const removeStorage = (key) => chrome.storage.local.remove(key);

371af0db081af70a368c9d84786d14c902360fa1
vucoffee2310 <vucoffee2310@gmail.com>
2025-09-28 07:54:05 +0700

    Delete khong directory

--- khong/services/storage.js
+++
@@ -1,3 +0,0 @@
-export const getStorage = async (k) => { const r = await chrome.storage.local.get(k); return r[k]; };
-export const setStorage = (k, v) => chrome.storage.local.set({ [k]: v });
-export const removeStorage = (k) => chrome.storage.local.remove(k);
--- khong/states/cards.js
+++
@@ -1,5 +0,0 @@
-import { getStorage, setStorage, removeStorage } from '../services/storage.js';
-const K = 'clickedCards_v1';
-export const getClickedCards = () => getStorage(K).then(v => v ?? {});
-export const addClickedCard = async (id) => { const c = await getClickedCards(); c[id] = true; return setStorage(K, c); };
-export const clearClickedCards = () => removeStorage(K);
